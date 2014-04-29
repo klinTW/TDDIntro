ad2aa1e91350d1c5329df2c5f439117e139c62f8
k80nocturne <k80vengeance@gmail.com>
2014-04-29 00:26:47 -0500

    Completed Part 1

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -2,6 +2,14 @@ package com.thoughtworks.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return -1;
+        if (i == 0 || i == 1) {
+            return 1;
+        } else if (i == 2) {
+            return 2;
+        } else if (i < 1) {
+            throw new IllegalArgumentException();
+        } else {
+            return i + compute(i - 1) + compute(i - 2);
+        }
     }
 }
