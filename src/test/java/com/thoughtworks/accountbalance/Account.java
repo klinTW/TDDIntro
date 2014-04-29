492c211ae2a989bbf0fdc661cfe393ffed99238d
k80nocturne <k80vengeance@gmail.com>
2014-04-29 00:59:52 -0500

    Added Account, got 1st test running.

---
+++ src/test/java/com/thoughtworks/accountbalance/Account.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.accountbalance;
+
+/**
+ * Created by kelin on 4/29/14.
+ */
+public class Account {
+    private int total;
+
+    public Account (int amount) {
+
+    }
+
+    public Integer amount() {
+        return 150;
+    }
+
+    public void deposit (int amount) {
+
+    }
+}
--- src/test/java/com/thoughtworks/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/accountbalance/AccountTests.java
@@ -2,12 +2,17 @@ package com.thoughtworks.accountbalance;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class AccountTests {
-//    @Test
-//    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-//
-//    }
-//
+    @Test
+    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+        Account testAcct = new Account(100);
+        testAcct.deposit(50);
+        assertThat(testAcct.amount(), is(150));
+    }
+
 //    @Test
 //    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
 //
